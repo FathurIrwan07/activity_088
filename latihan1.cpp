366f6cd4e9bb13a4096362646ea741b3b12b974f
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2023-03-08 12:11:50 +0700

    first commit

---
+++ latihan1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+ using namespace std;
+
+ int main(){ // Begin
+    // Numeric Luas,P,L
+    double Luas, P, L;
+    //display 'Masukkan Panjangnya = '
+    cout << "Masukkan Panjangnya = ";
+    //Accept p
+    cin >> P;
+    //display 'Masukkan Lebarnya = '
+    cout << "Masukkan Lebarnya = ";
+    //Accept l
+    cin >> L;
+    //Compute Luas = p*l
+    Luas = P*L;
+    //display "Luasnya = " +Luas
+    cout << "Luasnya = " << Luas;
+ } //end
---
+++ latihan2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+ using namespace std;
+
+ double Luas, P, L;
+ void input() {
+    cout << "Masukan Panjangnya =";
+    cin >> P;
+    cout << "Masukan Luasnya =";
+    cin >> L;
+ }
+ 
+ void proses() {
+    Luas = P*L;
+ }
+
+ void output() {
+    cout << "Luasnya =" << Luas;
+ }
+
+
+ int main() {
+    input();
+    proses();
+    output();
+ }
